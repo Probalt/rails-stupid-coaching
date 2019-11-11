76cf6d0525bcfaf7dff8c6618eced5e32879e6d2
balthazar lebret <balthazarlb@gmail.com>
2019-11-11 14:45:47 +0100

    form rails done

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get team" do
+    get pages_team_url
+    assert_response :success
+  end
+
+end
